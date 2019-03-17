037ce3b0d0c861c70d7de18b22b269ed113ffb5e
Farhan <fmshyder@gmail.com>
2019-03-17 19:12:14 -0400

    navbar added

--- hw4p5-react-address-book-app/address-book/src/App.js
+++ hw4p5-react-address-book-app/address-book/src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// react-bootstrap
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <Navbar className="bg-light justify-content-between">
+        <Navbar.Brand href="#home">Address Book</Navbar.Brand>
+          <Form inline>
+            <FormControl type="text" placeholder="Type here" className=" mr-sm-2" />
+            <Button type="submit">Search</Button>
+          </Form>
+        </Navbar>
+        
+
 
       </div>
     );
