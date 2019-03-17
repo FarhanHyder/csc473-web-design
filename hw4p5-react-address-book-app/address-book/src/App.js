23cf60fa0423c353491bd9b59129d6f2bdfa5702
Farhan <fmshyder@gmail.com>
2019-03-17 19:33:23 -0400

    default addresses added in state

--- hw4p5-react-address-book-app/address-book/src/App.js
+++ hw4p5-react-address-book-app/address-book/src/App.js
@@ -11,6 +11,51 @@ import Row from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 
 class App extends Component {
+
+  state = {
+    "addresses": [
+      {
+        "firstName": "Cathy",
+        "lastName": "Pierce",
+        "birthday": "9/14/1996",
+        "telephone": "200-910-8132"
+      },
+      {
+        "firstName": "Alfonso",
+        "lastName": "Cooley",
+        "birthday": "8/10/1973",
+        "telephone": "200-657-9362"
+      },
+      {
+        "firstName": "Victor",
+        "lastName": "Gordon",
+        "birthday": "8/3/1970",
+        "telephone": "200-661-9407"
+      },
+      {
+        "firstName": "Colleen",
+        "lastName": "Wright",
+        "birthday": "10/28/1967",
+        "telephone": "200-250-7949"
+      },
+      {
+        "firstName": "James",
+        "lastName": "Johnston",
+        "birthday": "5/11/1972",
+        "telephone": "200-645-3176"
+      },
+      {
+        "firstName": "Anna",
+        "lastName": "Reyes",
+        "birthday": "9/10/1975",
+        "telephone": "200-707-8670"
+      }
+    ]
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
@@ -23,7 +68,6 @@ class App extends Component {
           </Form>
         </Navbar>
         
-
 
       </div>
     );
