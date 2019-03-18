c7d358a92a5e726dfd20eeb57f534a7a4d587d4e
Farhan <fmshyder@gmail.com>
2019-03-17 21:02:10 -0400

    generateKey func added

--- hw4p5-react-address-book-app/address-book/src/App.js
+++ hw4p5-react-address-book-app/address-book/src/App.js
@@ -11,41 +11,53 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 
+const generateKey = () =>{
+  return Math.random().toString(36).replace('0.', '');
+};
+
 class App extends Component {
+
+  
 
   state = {
     "addresses": [
       {
+        "key": generateKey(),
         "firstName": "Cathy",
         "lastName": "Pierce",
         "birthday": "9/14/1996",
         "telephone": "200-910-8132"
       },
       {
+        "key": generateKey(),
         "firstName": "Alfonso",
         "lastName": "Cooley",
         "birthday": "8/10/1973",
         "telephone": "200-657-9362"
       },
       {
+        "key": generateKey(),
         "firstName": "Victor",
         "lastName": "Gordon",
         "birthday": "8/3/1970",
         "telephone": "200-661-9407"
       },
       {
+        "key": generateKey(),
         "firstName": "Colleen",
         "lastName": "Wright",
         "birthday": "10/28/1967",
         "telephone": "200-250-7949"
       },
       {
+        "key": generateKey(),
         "firstName": "James",
         "lastName": "Johnston",
         "birthday": "5/11/1972",
         "telephone": "200-645-3176"
       },
       {
+        "key": generateKey(),
         "firstName": "Anna",
         "lastName": "Reyes",
         "birthday": "9/10/1975",
@@ -53,6 +65,9 @@ class App extends Component {
       }
     ]
   }
+  
+
+  
 
   
 
